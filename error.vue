3b2e7cc975a6a8dc67ca7a84a142553615e29642
Тавабилов Денис Ринатович <d.tavabilov@prosoftsystems.ru>
2024-04-22 08:22:25 +0500

    end 40lessons

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <section>
+      {{ error }}
+      <h1 class="text-4xl mb-4">Oops {{ error.statusCode }}</h1>
+      <h2>
+        {{ error.statusMessage }}
+      </h2>
+    </section>
+  </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({ error: Object });
+</script>
